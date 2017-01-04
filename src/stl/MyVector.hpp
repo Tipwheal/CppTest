bb89becef5fc40fd1bcb5f2ab96863ae1190ee03
Tipwheal <674714966@qq.com>
2017-01-04 18:01:53 +0800

    奇怪的问题

--- src/stl/MyVector.hpp
+++ src/stl/MyVector.hpp
@@ -5,17 +5,26 @@
 
 using namespace std;
 
-typename<template>
+template<typename T>
 class MyVectorNode {
 public:
 	T *content;
 	MyVectorNode<T> *next;
 	MyVectorNode(T& t);
+	~MyVectorNode();
 };
 
 template<typename T>
 MyVectorNode<T>::MyVectorNode(T& t)
 	:content(t),next(NULL) {
+	cout << "in Node constr" << endl;
+}
+
+template<typename T>
+MyVectorNode<T>::~MyVectorNode() {
+	cout << "in node destr" << endl;
+	delete[] content;
+	delete[] next;
 }
 
 template<typename T>
@@ -32,13 +41,25 @@ public:
 };
 
 template<typename T>
-MyVector<T>::MyVector():first(new MyVectorNode()) {
-
+MyVector<T>::MyVector()
+	:first(NULL) {
+	cout << "in vec constr" << endl;
 }
 
 template<typename T>
 MyVector<T>::~MyVector() {
-
+	cout << "in vec destr" << endl;
+	cout << first << endl;
+	if(first == NULL)
+		return;
+	MyVectorNode<T> *cur = first;
+	MyVectorNode<T> *prev = NULL;
+	while(cur -> next != NULL) {
+		prev = cur;
+		cur = cur -> next;
+		delete[] prev;
+	}
+	delete[] cur;
 }
 
 template<typename T>
@@ -56,7 +77,15 @@ void MyVector<T>::push_back(T& t) {
 
 template<typename T>
 void MyVector<T>::clear() {
-
+	MyVectorNode<T> *cur = first;
+	MyVectorNode<T> *prev = NULL;
+	while(cur -> next != NULL) {
+		prev = cur;
+		cur = cur -> next;
+		delete[] prev;
+	}
+	delete[] cur;
+	first = NULL;
 }
 
 template<typename T>
@@ -66,7 +95,11 @@ void MyVector<T>::insert() {
 
 template<typename T>
 T& MyVector<T>::operator[](int index) {
-
+	MyVectorNode<T> *cur = first;
+	for(int i = 0;i<index;i++) {
+		cur = cur -> next;
+	}
+	return cur -> content;
 }
 
 #endif //_MY_VECTOR_H_
